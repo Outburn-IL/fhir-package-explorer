77d02df860e625d11d5c0ab274288bf7129780c2
mechanik-daniel <daniel@outburn.co.il>
2025-11-12 13:37:33 +0200

    chore: update ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,25 @@
-import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import { defineConfig } from 'eslint/config';
 
-
-export default defineConfig([
-  { files: ['**/*.{js,mjs,cjs,ts}'], plugins: { js }, extends: ['js/recommended'], ignores: ['**/node_modules/**', '**/dist/**', 'test/.test-cache/**'], languageOptions: { globals: globals.node }},
-  { files: ['**/*.{js,mjs,cjs,ts}'], languageOptions: { globals: globals.browser },
+export default [
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', 'test/.test-cache/**', 'test/manual/**']
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { 
+      globals: globals.node 
+    },
     rules: {
       indent: ['error', 2],
       semi: ['error', 'always'],
       quotes: ['error', 'single'],
       'no-unused-vars': ['warn'],
-      'no-console': 'off',
-      '@typescript-eslint/no-explicit-any': 'off'
-    } },
-  tseslint.configs.recommended,
-]);
+      'no-console': 'off'
+    }
+  },
+  ...tseslint.configs.recommended.map(config => ({
+    ...config,
+    files: ['**/*.{js,mjs,cjs,ts}']
+  }))
+];
